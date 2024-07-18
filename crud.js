2b20bd78641a438f379dee0ce4b511b0ff7ea1f7
Lucas <lucasaristiz789@gmail.com>
2024-07-17 21:24:10 -0500

    finally working users

--- crud.js
+++ crud.js
@@ -1,138 +1,178 @@
-
-function numerator() {
+class Utilities {
+  static numerator() {
     let x = 0;
-        return function() {
-            x += 1;
-            return x;
-        }
-};
+    return function () {
+      x += 1;
+      return x;
+    };
+  }
+}
 
-const newId = numerator()
+const newId = Utilities.numerator();
 
 class Persona {
-    constructor(username, password) {
-        this.id = newId
-        this.username = username;
-        this.password = password;
-        
-    };
+  constructor(username, password) {
+    this.id = newId();
+    this.username = username;
+    this.password = password;
+  }
 
-    static createUser(username, password) {
-        return new Persona(username, password);
-    }
+  //   static createUser(username, password) {
+  //     const newUser = new Persona(username, password);
+  //     return newUser;
+  //   }
 
-    static registerUser(username, password) {
-        let users = JSON.parse(localStorage.getItem('users')) || [];
-        users.push({username, password, role: 'regular'})
-        localStorage.setItem(this.username, JSON.stringify(this));
-    }
+  static registerUser(user) {
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    users.push(user);
+    localStorage.setItem("users", JSON.stringify(users));
+  }
 }
 
 class usuarioRegular extends Persona {
-    constructor(username, password) {
-        super(username, password)
-        this.Role = 'regular'
+  constructor(username, password) {
+    super(username, password);
+    this.Role = "regular";
+  }
+  static register(user) {
+    super.registerUser(user);
+  }
 
-    }
-    register() {
-        super.registerUser()
-    }
+  createReservation(details) {
+    let reservations =
+      JSON.parse(localStorage.getItem(`${this.username}_reservations`)) || [];
+    reservations.push(details);
+    localStorage.setItem(
+      `${this.username}_reservations`,
+      JSON.stringify(reservations)
+    );
+  }
 
-    createReservation(details) {
-        let reservations = JSON.parse(localStorage.getItem(`${this.username}_reservations`)) || [];
-        reservations.push(details);
-        localStorage.setItem(`${this.username}_reservations`, JSON.stringify(reservations));
-    }
-  
-    getReservations() {
-        return JSON.parse(localStorage.getItem(`${this.username}_reservations`)) || [];
-    }
+  getReservations() {
+    return (
+      JSON.parse(localStorage.getItem(`${this.username}_reservations`)) || []
+    );
+  }
 }
 
 class Administrador extends Persona {
-    constructor(username, password,) {
-        super(username, password)
-        this.Role = 'admin'
+  constructor(username, password) {
+    super(username, password);
+    this.Role = "admin";
+  }
 
-    }
+  static register(user) {
+    super.registerUser(user);
+  }
+  static createReservationForUser(username, details) {
+    let reservations =
+      JSON.parse(localStorage.getItem(`${username}_reservations`)) || [];
+    reservations.push(details);
+    localStorage.setItem(
+      `${username}_reservations`,
+      JSON.stringify(reservations)
+    );
+  }
+  deleteReservation(username, index) {
+    let reservations =
+      JSON.parse(localStorage.getItem(`${username}_reservations`)) || [];
+    reservations.splice(index, 1);
+    localStorage.setItem(
+      `${username}_reservations`,
+      JSON.stringify(reservations)
+    );
+  }
 
-    register() {
-        super.registerUser()
-    }
-
-    createReservationForUser(username, details) {
-        let reservations = JSON.parse(localStorage.getItem(`${username}_reservations`)) || [];
-        reservations.push(details);
-        localStorage.setItem(`${username}_reservations`, JSON.stringify(reservations));
-    }
-  
-    deleteReservation(username, index) {
-        let reservations = JSON.parse(localStorage.getItem(`${username}_reservations`)) || [];
-        reservations.splice(index, 1);
-        localStorage.setItem(`${username}_reservations`, JSON.stringify(reservations));
-    }
-  
-    updateReservation(username, index, details) {
-        let reservations = JSON.parse(localStorage.getItem(`${username}_reservations`)) || [];
-        reservations[index] = details;
-        localStorage.setItem(`${username}_reservations`, JSON.stringify(reservations));
-    }
-    
+  updateReservation(username, index, details) {
+    let reservations =
+      JSON.parse(localStorage.getItem(`${username}_reservations`)) || [];
+    reservations[index] = details;
+    localStorage.setItem(
+      `${username}_reservations`,
+      JSON.stringify(reservations)
+    );
+  }
 }
 
-Administrador.createReservationForUser()
+Administrador.createReservationForUser();
 
 class Auth {
-    static login(username, password) {
-      let users = JSON.parse(localStorage.getItem('users')) || [];
-      let user = users.find(u => u.username === username && u.password === password);
-      if (user) {
-        localStorage.setItem('loggedInUser', JSON.stringify(user));
-        return user;
-      }
-      return null;
-    }
-  
-    static logout() {
-      localStorage.removeItem('loggedInUser');
-    }
-  
-    static getLoggedInUser() {
-      return JSON.parse(localStorage.getItem('loggedInUser'));
-    }
-  }
-  
-  function registerUser() {
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    let role = document.getElementById('role').value;
-    console.log(registerUser)
-  
-    if (role === 'regular') {
-      usuarioRegular.registerUsa(username, password);
-    } else if (role === 'admin') {
-      Administrador.registerUsa(username, password);
-    }
-  }
-  
-  function loginUser() {
-    let username = document.getElementById('loginUsername').value;
-    let password = document.getElementById('loginPassword').value;
-    let user = Auth.login(username, password);
+  static login(username, password) {
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let user = users.find(
+      (u) => u.username === username && u.password === password
+    );
     if (user) {
-      document.getElementById('login').style.display = 'none';
-      document.getElementById('reservation').style.display = 'block';
+      localStorage.setItem("loggedInUser", JSON.stringify(user));
+      return user;
     }
+    return null;
   }
-  
-  function createReservation() {
-    let user = Auth.getLoggedInUser();
-    let details = document.getElementById('details').value;
-    if (user.role === 'regular') {
-      let userObj = new UsuarioRegular(user.username, user.password);
-      userObj.createReservation(details);
-    } else if (user.role === 'admin') {
-      let userObj = new Administrador(user.username, user.password);
-      userObj.createReservationForUser(user.username, details);
-    }
+
+  static logout() {
+    localStorage.removeItem("loggedInUser");
   }
+
+  static getLoggedInUser() {
+    return JSON.parse(localStorage.getItem("loggedInUser"));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault();
+  if (JSON.parse(localStorage.getItem("loggedInUser"))) {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("reservation").style.display = "block";
+  } else {
+    document.getElementById("login").style.display = "block";
+    document.getElementById("reservation").style.display = "none";
+  }
+});
+
+function registerUser() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let role = document.getElementById("role").value;
+  console.log(role);
+  console.log(username);
+  console.log(password);
+
+  if (role === "regular") {
+    const user = new usuarioRegular(username, password);
+    usuarioRegular.register(user);
+
+    alert("created successfully");
+  } else if (role === "admin") {
+    const user = new Administrador(username, password);
+    Administrador.register(user);
+    alert("created successfully");
+  } else {
+    console.log("apparently non-existent");
+  }
+}
+
+function loginUser() {
+  let username = document.getElementById("loginUsername").value;
+  let password = document.getElementById("loginPassword").value;
+  let user = Auth.login(username, password);
+  if (user) {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("reservation").style.display = "block";
+  }
+}
+
+function createReservation() {
+  let user = Auth.getLoggedInUser();
+  let details = document.getElementById("details").value;
+  if (user.Role === "regular") {
+    let userObj = new usuarioRegular(user.username, user.password);
+    userObj.createReservation(details);
+    document.getElementById("details").value = "";
+  } else if (user.Role === "admin") {
+    let userObj = new Administrador(user.username, user.password);
+    userObj.createReservationForUser(user.username, details);
+
+    document.getElementById("details").value = "";
+  }
+}
